bb4de66342e91bfe429b29ddea6253f4f56e0120
Yuto <yuto.basuke0508@icloud.com>
2021-07-23 01:52:42 +0900

    マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_153542) do
+ActiveRecord::Schema.define(version: 2021_07_22_163939) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,20 @@ ActiveRecord::Schema.define(version: 2021_07_22_153542) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "ramen_name", null: false
+    t.string "shop_name", null: false
+    t.string "place", null: false
+    t.integer "price", null: false
+    t.integer "soup_id", null: false
+    t.integer "noodle_id", null: false
+    t.text "caption", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,4 +61,5 @@ ActiveRecord::Schema.define(version: 2021_07_22_153542) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
