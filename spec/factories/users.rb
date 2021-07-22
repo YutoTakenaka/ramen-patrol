70004975cf89e0a02f22f87e3b5d9ee128537a04
Yuto <yuto.basuke0508@icloud.com>
2021-07-23 00:36:40 +0900

    モデル単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
+    nickname {Faker::Internet.username}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
     
+    after(:build) do |user|
+      user.image.attach(io: File.open('app/assets/images/star.png'), filename: 'star.png')
+    end
   end
 end
