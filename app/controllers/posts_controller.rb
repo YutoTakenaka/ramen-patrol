56b5eb7b8d2214f230981362f704e0e66336dc37
Yuto <yuto.basuke0508@icloud.com>
2021-07-24 01:05:16 +0900

    コントローラー

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,22 @@ class PostsController < ApplicationController
     @posts = Post.all.order('created_at DESC')
   end
   
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image ,:ramen_name ,:shop_name ,:place ,:price ,:soup_id ,:noodle_id ,:caption).merge(user_id: current_user.id)
+  end
 end
