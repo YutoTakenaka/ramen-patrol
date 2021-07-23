2ad0d05c1b6b981bdd86509f160465e899dd2542
Yuto <yuto.basuke0508@icloud.com>
2021-07-23 23:55:23 +0900

    active_hashのmodel追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :soup
+    belongs_to :noodle
+
   with_options presence: true do
     validates :ramen_name
     validates :shop_name
@@ -12,5 +16,11 @@ class Post < ApplicationRecord
     validates :caption
     validates :image
   end
+
+  with_options numericality: { other_than: 1,message: "can't be blank" }  do
+    validates :soup_id
+    validates :noodle_id
+  end
+
   
 end
