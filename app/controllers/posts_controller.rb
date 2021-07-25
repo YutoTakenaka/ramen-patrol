4dfa635a268bb0b648417c356f8bc21fe7883f87
Yuto <yuto.basuke0508@icloud.com>
2021-07-25 20:41:04 +0900

    コントローラー

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :set_post ,only: [:show ,:edit ,:update]
+  before_action :set_post ,only: [:show ,:edit ,:update ,:destroy]
+  before_action :move_to_index ,only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.order('created_at DESC')
@@ -34,12 +35,21 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:image ,:ramen_name ,:shop_name ,:place ,:price ,:soup_id ,:noodle_id ,:caption).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    redirect_to root_path unless current_user.id == @post.user_id
+  end
+  
   def set_post
     @post = Post.find(params[:id])
   end
